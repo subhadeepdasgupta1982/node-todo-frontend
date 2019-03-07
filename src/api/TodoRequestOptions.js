c566909b0f33177444f4cdfeefc5cfec0c98e09d
subhadeepdasgupta1982 <38577839+subhadeepdasgupta1982@users.noreply.github.com>
2019-03-07 12:10:44 +0530

    Update TodoRequestOptions.js

--- src/api/TodoRequestOptions.js
+++ src/api/TodoRequestOptions.js
@@ -4,7 +4,7 @@ class TodoRequestOptions {
 	constructor(host, todoUrl) {
 		this.host = host ? host : 'http://' + 
  			(process.env.TODO_BACKEND ? process.env.TODO_BACKEND : '52.42.111.188') + ':' + 
- 			(process.env.TODO_BACKEND_PORT ? process.env.TODO_BACKEND_PORT : '3000') + '/';
+ 			(process.env.TODO_BACKEND_PORT ? process.env.TODO_BACKEND_PORT : '3001') + '/';
 		this.todoUrl = todoUrl ? todoUrl : 'api/Todos/';
 	}
 
